c32b668d7b3576c893c378fdc8eb82d38d3f49f6
Lamiae7 <107724048+Lamiae7@users.noreply.github.com>
2022-11-04 21:34:47 +0100

    Update Weapon.py

--- Weapon.py
+++ Weapon.py
@@ -1,46 +1,43 @@
-#from re import X
-
-
 class Weapon :
     def __init__(self, ammunitions=int, range=int):
         self.ammunitions = ammunitions
         self.range = range
 
-    def fire_at(self, x, y, z) :
+    def fire_at(self, x=int, y=int, z=int) :
         self.x = x
         self.y = y
         self.z = z
-        print('ATTACK')
+        print("ATTACK ON (",x,y,z,')')
 
 
 class Lance_missiles_antisurface(Weapon) :
     rayon_action = 30
-    nbr_munitions = 40
+    nbr_ammunitions = 40
 
     def fire_at(self, x, y, z):
-        assert Lance_missiles_antisurface.nbr_munitions >0, "NoAmmunitionError"
+        assert Lance_missiles_antisurface.nbr_ammunitions >0, "NoAmmunitionError"
         assert z==0, "OutOfRangeError"
         Lance_missiles_antisurface.nbr_munitions-=1
         return super().fire_at(x, y, z)     
 
 class Lance_missiles_antiair(Weapon) :
     rayon_action = 40
-    nbr_munitions = 15
+    nbr_ammunitions = 15
 
     def fire_at(self, x, y, z):
-        assert Lance_missiles_antiair.nbr_munitions >0, "NoAmmunitionError"
+        assert Lance_missiles_antiair.nbr_ammunitions >0, "NoAmmunitionError"
         assert z>0,"OutOfRangeError"
-        Lance_missiles_antiair.nbr_munitions-=1
+        Lance_missiles_antiair.nbr_ammunitions-=1
         return super().fire_at(x, y, z)
 
 class Lance_torpilles(Weapon) :
     rayon_action = 20
-    nbr_munitions = 15
+    nbr_ammunitions = 15
 
     def fire_at(self, x, y, z):
-        assert Lance_torpilles.nbr_munitions >0, "NoAmmunitionError"
+        assert Lance_torpilles.nbr_ammunitions >0, "NoAmmunitionError"
         assert z<=0, "OutOfRangeError"
-        Lance_torpilles.nbr_munitions-=1
+        Lance_torpilles.nbr_ammunitions-=1
         return super().fire_at(x, y, z)
         
 
@@ -48,7 +45,9 @@ class Lance_torpilles(Weapon) :
 
 
 
-
+#PO = Lance_missiles_antisurface(3, 4)
+#print(PO.fire_at(1,1,1)==assertRaises("OutOfRangeError"))
+#print(PO.range)
 #return '{}: {} - {}'.format(premier_à_ecrie, deuxième_à_ecrire, 3_eme)
 #on peut aussi écrire :
 #return f"{premier}: {deuxième} - {troixième}
